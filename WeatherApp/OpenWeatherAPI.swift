d04781c802a597f9d386e23077f0e3a993f1b31b
ShashankKannam <kannamshashank19@gmail.com>
2016-11-18 01:05:17 -0600

    Update OpenWeatherAPI.swift
    Add API key to use

--- WeatherApp/OpenWeatherAPI.swift
+++ WeatherApp/OpenWeatherAPI.swift
@@ -12,7 +12,7 @@ let BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?"
 let LATITUDE = "lat="
 let LONGITUDE = "&lon="
 let APP_ID = "&appid="
-let API_KEY = "84367f2140c8f9873aa45330eb7f2bab"
+let API_KEY = ""
 
 let CURRENT_URL = "\(BASE_URL)\(LATITUDE)\(Location.sharedLocation.latitude!)\(LONGITUDE)\(Location.sharedLocation.longitude!)\(APP_ID)\(API_KEY)"
 
@@ -22,7 +22,7 @@ let FBASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?"
 let FLATITUDE = "lat="
 let FLONGITUDE = "&lon="
 let FAPP_ID = "&appid="
-let FAPI_KEY = "84367f2140c8f9873aa45330eb7f2bab"
+let FAPI_KEY = ""
 let FCURRENT_URL = "\(FBASE_URL)\(FLATITUDE)\(Location.sharedLocation.latitude!)\(FLONGITUDE)\(Location.sharedLocation.longitude!)&cnt=10&mode=json\(FAPP_ID)\(FAPI_KEY)"
 
 //typealias DownloadCompleted = () -> ()
